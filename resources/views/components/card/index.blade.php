cc01acc3e4668b86f239afd64c6d54f67fa3dcfc
AJ Meireles <alvaro.meireles@live.com>
2023-07-23 19:44:45 -0300

    enhancements on the first components

--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -12,17 +12,17 @@
     $color = $icon === 'arrow-up' ? 'green' : 'red';
 @endphp
 
-<div class="relative overflow-hidden rounded-lg bg-white px-4 pt-5 shadow sm:px-6 sm:pt-6">
+<div class="relative overflow-hidden rounded-lg bg-white p-10 shadow">
     <dt>
-        <div class="absolute rounded-md bg-indigo-500 p-3">
-            @svg($symbol, "h-6 w-6 text-white")
+        <div class="absolute rounded-md p-3">
+            @svg($symbol, "h-8 w-8 text-indigo-600")
         </div>
         @if ($title)
             <p class="ml-16 truncate text-sm font-medium text-gray-500">{{ $title }}</p>
         @endif
     </dt>
     <div class="hidden text-red-500 text-green-500"></div>
-    <dd class="ml-16 flex items-baseline pb-6 sm:pb-7">
+    <dd class="ml-16 flex items-center">
         <p class="text-2xl font-semibold text-gray-900">{{ $value }}</p>
         <span class="ml-2 flex items-baseline text-sm font-semibold">
             @if ($icon)
--- resources/views/components/table/thread.blade.php
+++ resources/views/components/table/thread.blade.php
@@ -1,3 +1,3 @@
-<thead {{ $attributes->merge(['class' => 'bg-gray-50']) }}>
+<thead {{ $attributes->merge(['class' => 'bg-gray-50 uppercase']) }}>
 {{ $slot }}
 </thead>
